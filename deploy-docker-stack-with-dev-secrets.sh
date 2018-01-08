ae4b6d77b5a56828e1f6150f3ec549caf8349a20
mithilmanjrekar <33927380+mithilmanjrekar@users.noreply.github.com>
2018-01-08 18:19:29 +0530

    Update and rename start-docker-swarm-stack.sh to deploy-docker-stack-with-dev-secrets.sh

--- deploy-docker-stack-with-dev-secrets.sh
+++ deploy-docker-stack-with-dev-secrets.sh
@@ -5,12 +5,13 @@ echo "root" | docker secret create mysql_user -
 echo "rootroot" | docker secret create mysql_password -
 echo "testnpm" | docker secret create mysql_database -
 
-docker stack deploy -c docker-compose-stack.yml dragsters_overlay
+docker stack deploy -c docker-stack.yml dragsters_overlay
  
 docker stack ls
+docker network ls
+docker secret ls
 
-docker stack rm dragsters_overlay
-
+# docker stack rm dragsters_overlay
 # to check if it is up or go to browser
 # curl -s 'http://localhost:4040/api/tunnels'
 # curl -s  'http://localhost:3000'
